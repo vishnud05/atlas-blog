dd43bb6aa1db34b5f7195361dcb96819bd01d1af
Vishnu Dev Aggarwal <vishnu.3b@gmail.com>
2024-05-28 13:38:52 +0530

    add dropdown menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,15 @@ import { signOut, useSession } from "next-auth/react";
 import { filterLinks } from "@/utils/helperFunctions";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar = () => {
   const { data, status } = useSession();
@@ -26,11 +35,12 @@ const Navbar = () => {
   return (
     <header className="flex items-center justify-between bg-transparent px-4 py-3 shadow-sm dark:bg-gray-950">
       <Link className="flex items-center gap-2" href="/">
-        <span className="text-2xl font-bold text-gray-900 dark:text-gray-50">
+        <Image alt="atlas-logo" src="atlas-logo.svg" width={30} height={30} />
+        <span className="hidden md:block text-2xl font-bold text-gray-900 dark:text-gray-50">
           A T L A S
         </span>
       </Link>
-      <div className="relative w-full max-w-[300px]">
+      <div className="relative md:w-full max-w-[300px]">
         <SearchIcon className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-500 dark:text-gray-400" />
         <Input
           className="h-8 w-full rounded-md border-gray-200 bg-gray-100 pl-8 text-sm focus:border-gray-400 focus:ring-0 dark:border-gray-800 dark:bg-gray-800 dark:text-gray-50"
@@ -38,6 +48,41 @@ const Navbar = () => {
           type="search"
         />
       </div>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="md:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M6 9l6 6 6-6" />
+          </svg>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="md:hidden">
+          {links.map((link) => (
+            <DropdownMenuItem>
+              <Link
+                className={`text-md text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50$`}
+                href={
+                  link.title === "Profile"
+                    ? `${link.href}/${data?.user._id}`
+                    : link.href
+                }
+                key={link.href}
+              >
+                {link.title}
+              </Link>
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
 
       <div className="flex items-center gap-6 text-sm font-medium">
         <nav className="hidden items-center md:flex gap-6">
@@ -57,7 +102,7 @@ const Navbar = () => {
         </nav>
         <Button
           onClick={handleAuthentication}
-          className="ml-6 inline-flex h-8 items-center justify-center rounded-md bg-gray-900 px-4 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+          className=" inline-flex h-8 items-center justify-center rounded-md bg-gray-900 px-4 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
         >
           {status === "authenticated" ? "Sign Out" : "Get Started"}
         </Button>
